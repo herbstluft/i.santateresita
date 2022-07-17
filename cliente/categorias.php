50a3682adfb73b33f6cbfceb74d84897d3638559
herbstluft <herbstluftwm.28@gmail.com>
2022-07-17 00:12:19 -0500

    Arreglo de diseño desacomodado

--- cliente/categorias.php
+++ cliente/categorias.php
@@ -16,14 +16,9 @@
 
     <style>
 
-     .col-lg-4 {
-    margin-bottom: 1.5rem;
-    text-align: center;
-    }
-
     .fonts{
     font-size: 17px;
-  }
+    }
   
     </style>
 
@@ -36,132 +31,176 @@
     <canvas class="orb-canvas" width="313" height="781" style="touch-action: none; cursor: inherit;"></canvas>
 
 
+<div class="container py-3">
 
-  <div class="container marketing py-3">
+
+<!--Barra de navegacion -->
+
+<nav class="navbar navbar-expand-lg barra sticky-top" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.php"><img src="../bootstrap/img/logo.png" style="width: 30%;"/> &ensp; Santa Teresita</a>
     
+    <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-
-   
-   <!--Barra de navegacion -->
-
-   <nav class="navbar navbar-expand-lg barra sticky-top" >
-    <div class="container-fluid">
-      <a class="navbar-brand" href="../index.html"><img src="../bootstrap/img/logo.png" style="width: 30%;"/> &ensp; Santa Teresita</a>
-      
-      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="navbar-collapse collapse" id="navbarColor03" >
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <center>
-            <b>
-          <li class="nav-item i">
-            <a class="nav-link fonts" aria-current="page" href="../index.html">Inicio</a>
-          </li>
-          <li class="nav-item i">
-            <a class="nav-link fonts active" href="categorias.html">Categorias</a>
-          </li>
-          <li class="nav-item i">
-            <a class="nav-link fonts" href="#">Cita</a>
-          </li>
-          <li class="nav-item i">
-            <a class="nav-link fonts" href="#">Nosotros</a>
-          </li>
-          </b>
-        </center> &ensp; &ensp; &ensp;  &ensp; &ensp;  &ensp; &ensp;  &ensp; &ensp;
-        </ul>
-        <form class="d-flex" role="search">
-          <br><br>
-          <a href="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="50" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-          </a>
-          </svg>  &ensp; &ensp;  &ensp; &ensp;  
-          <button class="btn sombras iniciar" type="submit">
-            <a class="a" href="../login.html">  Iniciar Session   </a></button>
-        </form>
-      </div>
+    <div class="navbar-collapse collapse" id="navbarColor03" >
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <center>
+          <b>
+        <li class="nav-item i">
+          <a class="nav-link active fonts" aria-current="page" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item i">
+          <a class="nav-link fonts" href="cliente/categorias.php">Categorias</a>
+        </li>
+        <li class="nav-item i">
+          <a class="nav-link fonts" href="#">Cita</a>
+        </li>
+        <li class="nav-item i">
+          <a class="nav-link fonts" href="#">Nosotros</a>
+        </li>
+        </b>
+      </center> &ensp; &ensp; &ensp;  &ensp; &ensp;  &ensp; &ensp;  &ensp; &ensp;
+      </ul>
+      <form class="d-flex" role="search">
+        <br><br>
+        
+        <a href="">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="50" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+        </svg>
+      </a>  &ensp; &ensp;  &ensp; &ensp;  
+        <button class="btn sombras iniciar" type="submit">
+          <a class="a" href="login.php">  Iniciar Session   </a></button>
+      </form>
     </div>
-  </nav>
- 
+  </div>
+</nav>
+
+
+
+<br>
+
+
+  <!--Contenido de la pagina-->
+<div class="container">
+
   
- 
-  <br>
-
-
-    <main>
-
-    <div class="row">
-      <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
         <br>
-        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="http://s4.thingpic.com/images/zZ/HBdH8erKPYyTHDe1j9nX7AVz.png" alt="">
         <br><br>
         <h2 class="fw-normal">Estomacal</h2> <br>
-        <p>Los medicamentos para la acidez estomacal, suprimen el ácido estomacal. Bloquear el ácido y otras secreciones del estómago también puede bloquear la absorción de la vitamina B-12.</p>
+        <p>Bloquear el ácido y otras secreciones del estómago también puede bloquear la absorción de la vitamina B-12.</p><br>
         <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
       </div>
-
-      <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
+    
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
         <br>
-        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://www.prensalibre.com/wp-content/uploads/2019/08/shutterstock_1187048224.jpg?quality=52&w=760&h=430&crop=1" alt="">
+        <br><br>
+        <h2 class="fw-normal">Dolores</h2> <br>
+        <p>Calman el dolor porque cortan la inflamación que lo provoca. </p><br><br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+  </div>
+
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2018/08/07/21/57/fever-3590869__340.jpg" alt="">
+        <br><br>
+        <h2 class="fw-normal">Fiebre</h2> <br>
+        <p>Estos medicamentos están indicados para combatir y disminuir el nivel de la temperatura corporal.<p><br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+    
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2015/08/02/14/27/vitamin-b-871135__340.jpg" alt="">
         <br><br>
         <h2 class="fw-normal">Suplementos Alimenticios</h2> <br>
         <p>Los suplementos alimenticios son productos destinados a cubrir, complementar o incrementar la ingesta de determinados nutrientes en la dieta.</p><br>
         <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
       </div>
-    </div>
+  </div>
 
 
-    <div class="row">
-        <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
-          <br>
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
-          <br><br>
-          <h2 class="fw-normal">Dolor</h2> <br>
-          <p>Son medicinas que reducen o alivian los dolores de cabeza, musculares, artríticos o muchos otros achaques y dolores.</p><br>
-          <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
-        </div>
-  
-        <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
-          <br>
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
-          <br><br>
-          <h2 class="fw-normal">Fiebre</h2> <br>
-          <p>La fiebre es la elevación de la temperatura 
-            corporal provocada por una desviación al alza inducida 
-            por citocinas del nivel predeterminado del centro 
-            termorregulador del hipotálamo.</p>
-          <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
-          <br>
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
-          <br><br>
-          <h2 class="fw-normal">Estomacal</h2> <br>
-          <p>Los medicamentos para la acidez estomacal, suprimen el ácido estomacal. Bloquear el ácido y otras secreciones del estómago también puede bloquear la absorción de la vitamina B-12.</p>
-          <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
-        </div>
-  
-        <div class="col-10 offset-1 offset-sm-1 col-lg-4 offset-lg-1 sombras text-center">
-          <br>
-          <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_960_720.jpg" alt="">
-          <br><br>
-          <h2 class="fw-normal">Suplementos Alimenticios</h2> <br>
-          <p>Los suplementos alimenticios son productos destinados a cubrir, complementar o incrementar la ingesta de determinados nutrientes en la dieta.</p><br>
-          <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
-        </div>
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2020/03/29/15/35/coronavirus-4981176__340.png" alt="">
+        <br><br>
+        <h2 class="fw-normal">Gripa y tos</h2> 
+        <p>Se utilizan para reducir y aliviar los síntomas de la gripe como garganta irritada, la coriza y/o la tos.<p>
+        <br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
       </div>
     
       
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2013/04/22/01/13/wound-106374__340.jpg" alt="">
+        <br><br>
+        <h2 class="fw-normal">Material de curación </h2> <br>
+        <p>Suele ser desechable pues entra en contacto con bacterias y virus que le impiden ser reutilizado.</p><br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+  </div>
 
 
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2012/04/10/17/40/vitamins-26622__340.png" alt="">
+        <br><br>
+        <h2 class="fw-normal">Vitaminas y minerales</h2> <br>
+        <p>Estimulan el funcionamiento del sistema inmunitario, y ayudan a las células y a los órganos a desempeñar sus respectivas funciones.<p>
+        <br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+    
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://img.freepik.com/vector-premium/linda-coleccion-medicamentos-aislada-sobre-fondo-rosa_598722-13.jpg?w=2000" alt="">
+        <br><br>
+        <h2 class="fw-normal">Sueros </h2> <br>
+        <p>Ayudan a tratar el acné, el envejecimiento, las manchas, poros abiertos, flacidez cutánea, etc.</p><br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+  </div>
 
-</main>
 
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2017/06/24/22/32/lingerie-2439134__340.jpg" alt="">
+        <br><br>
+        <h2 class="fw-normal">Bienestar Sexual</h2> <br>
+        <p>estos productos permiten proteger la zona íntima de la mujer ante cualquier fricción no deseada.<p>
+        <br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+    
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn.pixabay.com/photo/2020/05/21/14/08/pregnant-woman-5200901__340.jpg" alt="">
+        <br><br>
+        <h2 class="fw-normal">Ginecologia</h2> <br>
+        <p>Estos productos ayudan a un mejor tratamiento para la mujer en el proceso de embarazo</p><br>
+        <p><a class="btn sombras" href="#">Ver productos &raquo;</a></p>
+      </div>
+  </div>
+
+
+</div>
+</div>
   
     <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>    
     <script src="../bootstrap/js/bootstrap.min.js"></script>
