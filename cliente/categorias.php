d4250cc52953da036cd78476887e0d1f8494eaf8
viktormiller777 <cabellordz15@gmail.com>
2022-07-31 12:50:08 -0500

    categorias

--- cliente/categorias.php
+++ cliente/categorias.php
@@ -32,14 +32,15 @@ session_start();
     
   </head>
   <body>
-    
-
-  <div class="fullscreen-container">
+          <!-- Fondo de video -->
+<div class="fullscreen-container">
     <video loop muted autoplay poster="dist/img/office.jpg" class="fullscreen-video">
         <source src="https://player.vimeo.com/external/641767478.hd.mp4?s=d1e3f6e09192708d3ac42cc85979c361242f11f5&profile_id=174&oauth_token_id=1027659655" type="video/mp4">
 
     </video>
 </div>
+<!-- fin de video -->
+    
 
 
 <div class="container py-3">
