cfad9ef33309f9be6f51fdf5630b05f1ecf9dd7b
viktormiller777 <cabellordz15@gmail.com>
2022-08-09 12:27:41 -0500

    conexiones de productos

--- cliente/productos/fiebre.php
+++ cliente/productos/fiebre.php
@@ -1,10 +1,18 @@
 <?php 
 //ocultar warnings
 error_reporting(E_ERROR | E_PARSE);
-include('../../admin/conexion.php');
+use MyApp\data\Database;
+use MyApp\query\ejecuta;
+use MyApp\query\Select;
+          
+
+require("../../vendor/autoload.php");
+
+$query = new Select();
+
 session_start();
-$obj= new conexion();
-$productos=$obj->consultar("select * from  productos inner JOIN categoria on productos.id_cat=categoria.id_cat WHERE categoria.categoria='Fiebre';");
+
+$productos=$query->seleccionar("select * from  productos inner JOIN categoria on productos.id_cat=categoria.id_cat WHERE categoria.categoria='Fiebre';");
 ?>
 
 <!doctype html>
