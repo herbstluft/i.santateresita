8b88c0fad15bb6b1870107946096eed0c97aa962
herbstluft <herbstluftwm.28@gmail.com>
2022-08-14 18:17:08 +0200

    farmacia

--- admin/index.php
+++ admin/index.php
@@ -1,73 +1,16 @@
 <?php
-
 use MyApp\data\Database;
 use MyApp\query\ejecuta;
 use MyApp\query\Select;
 
-
 require("../vendor/autoload.php");
-
-$query = new Select();
-$obj = new ejecuta();
+//ocultar warnings
 error_reporting(E_ERROR | E_PARSE);
-
 session_start();
 
-if($_POST){
-
-  print_r($_POST);
-  $nom=$_POST['nom'];
-  $imagen=$_FILES['imagen']['name'];
-  $precio=$_POST['precio'];
-  $fecha=$_POST['fecha_v'];
-  $formula=$_POST['formula'];
-  $categoria=$_POST['categoria'];
-  $desc=$_POST['desc'];
-
-
-
-  // Guardar imagen
-  $imagen=$_FILES['imagen']['name'];
-  /////////  Crear imagenes temporales
-  $imagen_temporal=$_FILES['imagen']['tmp_name'];
-  //mover  imagen a carpeta imagenes
-  move_uploaded_file($imagen_temporal,"imagenes/".$imagen);
-
-
-  $insert="INSERT INTO `productos` (`nom_producto`,`imagen`,`precio`,`fecha_vencimiento`,`id_cat`,`formula`,`descripcion`) VALUES ('$nom','$imagen','$precio','$fecha','$categoria','$formula','$desc')";
-  $obj->ejecutar($insert);
-}
-
-
-//Seleccionar datos de los productos
-$productos=$query->seleccionar("SELECT todo.id, todo.nombre, todo.precio, todo.fecha, todo.formula, todo.descripcion, todo.imagen, todo.categoria from
-(SELECT productos.id_producto as id, productos.nom_producto as nombre, productos.precio as precio, productos.fecha_vencimiento as fecha, productos.formula as formula,
-productos.id_cat as id_cat, productos.descripcion as descripcion, productos.IMAGEN as imagen, categoria.categoria as categoria from productos 
-inner join categoria on categoria.id_cat=productos.id_cat) as todo");
-
-
-
-if($_GET){
-  $id=$_GET['borrar'];
- 
-  //borrar_imageen
-  $imagen=$query->seleccionar("SELECT imagen FROM `productos` where id_producto=".$id);
-  //borrar la imagen temporal (de la carpeta)
-  unlink("imagenes/".$imagen[0]->imagen);
-  //borrar producto
-  $sql="DELETE FROM productos WHERE `productos`.`id_producto` =".$id;
-  $obj->ejecutar($sql);
-  
-}
-
-
 ?>
-
-<?php 
-if(isset($_SESSION['admin'])){
-?>
-<!DOCTYPE html>
-<html lang="es">
+<!doctype html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -93,9 +36,14 @@ if(isset($_SESSION['admin'])){
     
   </head>
   <body>
-    
+<!-- Fondo de video -->
+<div class="fullscreen-container">
+    <video loop muted autoplay poster="" class="fullscreen-video">
+        <source src="../bootstrap/img/back.mp4" type="video/mp4">
 
-  
+    </video>
+</div>
+<!-- fin de video -->
 
 
 <div class="container py-3">
@@ -105,157 +53,144 @@ if(isset($_SESSION['admin'])){
 
 <nav class="navbar navbar-expand-lg barra sticky-top" >
   <div class="container-fluid">
-    <a class="navbar-brand" ><img src="../bootstrap/img/logo.png" style="width: 30%;"/> &ensp; Santa Teresita</a>
+    <a class="navbar-brand" href="../index.php"><img src="../bootstrap/img/logo.png" style="width: 30%;"/> &ensp; Santa Teresita</a>
     
     <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="navbar-collapse collapse" id="navbarColor03" >
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0  col-2 offset-6 col-md-2 offset-md-5" >
-        <center>
-          <b>
-        <li class="nav-item i text-center">
-          <a class="nav-link active fonts" aria-current="page" href="index.php">Inicio</a>
-        </li>
-        </b>
-      </center>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+   
+       
+      
       </ul>
-      <form class="d-flex" role="search">
+      <form class="d-flex" action="categorias.php" method="POST" role="search">
         <br><br>
         
-        &ensp; &ensp;  &ensp; &ensp; &ensp; &ensp;  
-        <?php
-        if(isset($_SESSION['admin'])){
+       &ensp;  &ensp; &ensp;  &ensp; &ensp; 
+      <?php
+           if($_SESSION['admin']){
             ?>
-              <button class="btn sombras iniciar col-8 " type="submit" style="height: 50px; position: relative; top:30px" name="cerrar_session">
-          <a class="a" href="../src/scripts/logout.php">  <?php echo "Cerrar Session" ?> </a></button>
+           <button class="btn sombras iniciar" id="registrarme" type="submit">
+              <a class="a" href="../src/scripts/logout.php"  > Cerrar Session   </a></button>
             <?php
             } 
+            else{
+              ?>
+               <button class="btn sombras iniciar" id="registrarme" type="submit">
+              <a class="a" href="../registro/login.php"  >  Iniciar Session   </a></button>
+              <?php
+            }
             ?>
-          
       </form>
     </div>
   </div>
 </nav>
-  </div>
-  
-  <!-- Fondo de video -->
-  <div class="fullscreen-container">
-    <video loop muted autoplay poster="" class="fullscreen-video">
-        <source src="../bootstrap/img/back.mp4" type="video/mp4">
-
-    </video>
-</div>
-<!-- fin de video -->
 
 <br>
 
 
   <!--Contenido de la pagina-->
-  <link rel="stylesheet" href="/i.santateresita/bootstrap/css/index.css">
 <div class="container">
 
-  <div class="sombras" style="padding:4% ;">
-    <div class="card-header ">
-      <center><h2>Datos del producto</h2></center>
-    </div>
-    <div class="card-body">
-
-      <form action="index.php" method="post" enctype="multipart/form-data">
-        Nombre: <br>
-        <input class="form-control" type="text" name="nom" required>
+  
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://static.vecteezy.com/system/resources/previews/002/693/886/non_2x/vaccine-bottle-gradient-linear-icon-vial-with-drug-and-syringe-pharmaceutical-remedy-medication-for-flu-thin-line-color-symbols-modern-style-pictogram-isolated-outline-drawing-vector.jpg" alt="">
         <br><br>
-        Imagen:
-        <input class="form-control" type="file" name="imagen" required>
-        <br>
-        Precio:
-        <input class="form-control" type="number" name="precio" required>
-        <br>
-        Fecha Vencimiento;
-        <input class="form-control" type="date" name="fecha_v">
-        <br> 
-        Formula: <br>
-        <input class="form-control" type="text" name="formula">
-        <br><br>
-        Categoria: <br>
-        <select class="form-select" aria-label="Default select example" name="categoria" required>
-          
-          <option value="1">Estomacal</option>
-          <option value="2">Dolor</option>
-          <option value="3">Fiebre</option>
-          <option value="4">Suplementos Alimenticios</option>
-          <option value="5">Gripa y tos</option>
-          <option value="6">Material de curacion</option>
-          <option value="7">Vitaminas y minerales</option>
-          <option value="8">Sueros</option>
-          <option value="9">Bienestar sexual</option>
-          <option value="10">Ginecologia</option>
-        </select>
-        <br>
-        Descripcion: <br>
-        <input class="form-control" type="text" name="desc" minlength="20" maxlength="50" required>
-        <br> <br>
-        <button class="btn sombras registrarme" type="submit" id="registrarme" name="enviar" style="font-size:25px;">Publicar</button>
-
-    </form>
-    </div>
+        <h2 class="fw-normal">Publicar</h2> <br>
+        <h5>Publicar productos en la tienda en linea</h5><br>
+        <p><a class="btn sombras" id="registrarme" href="acciones/publicar.php">Publicar &raquo;</a></p>
+      </div>
     
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f50d.png" alt="">
+        <br><br>
+        <h2 class="fw-normal">Buscar</h2> <br>
+        <h5>Busca y elimina productos.</h5><br><br>
+        <p><a class="btn sombras" id="registrarme" href="/index.php">Ir &raquo;</a></p>
+      </div>
+  </div>
+
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://us.123rf.com/450wm/rastudio/rastudio1802/rastudio180200302/96103932-hombre-caucásico-joven-que-camina-con-bolsas-de-compras-de-plástico-con-verduras-saludables-y-frutas.jpg?ver=6" alt="">
+        <br><br>
+        <h2 class="fw-normal">Modo Cliente</h2> <br>
+        <h5>Mirate como un cliente</h5><br>
+        <p><a class="btn sombras" id="registrarme" href="../index.php">Ir &raquo;</a></p>
+      </div>
+    
+      
+  
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://img.freepik.com/vector-gratis/ilustracion-coleccion-medicos-enfermeras-dibujos-animados_23-2148920403.jpg?w=2000" alt="">
+        <br><br>
+        <h2 class="fw-normal">Doctores</h2> <br>
+        <h5>Ver los doctores que atienden.</h5><br>
+        <p><a class="btn sombras" id="registrarme" href="productos/suplementos.php">Ir &raquo;</a></p>
+      </div>
   </div>
   
-    <br><br>
-  <table class="sombras table table-hover table-responsive">
-    <thead>
-      <tr class="text-center">
+  <div class="row">
+      <div class="col-12 col-xs-5 col-md-12 col-lg-5 offset-2 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="http://4.bp.blogspot.com/-cVO8CTsGC1w/Um0SOZz6fDI/AAAAAAAAAok/dopUSBWisYs/s1600/Reporte+I.png" alt="">
+        <br><br>
+        <h2 class="fw-normal">Reportes</h2> <br>
+        <h5>Ver reporte de venta diario</h5><br>
+        <p><a class="btn sombras" id="registrarme" href="productos/estomacal.php">Publicar &raquo;</a></p>
+      </div>
+    
+      
+      <div class="col-12 col-xs-12 col-md-12 col-lg-5 sombras text-center space">
+        <br>
+        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://img.freepik.com/vector-premium/creatividad-e-imaginacion-crear-contenido-inspiracion-escritor-o-creador-nueva-idea-pensar-pensar-concepto-lluvia-ideas-hombre-motivado-sentado-columpio-dentro-idea-bombilla-usando-nube-dibujo-lapiz_212586-891.jpg" alt="">
+        <br><br>
+        <h2 class="fw-normal">Creadores</h2> <br>
+        <h5>Ver los creadores de la paginas</h5><br><br>
+        <p><a class="btn sombras" id="registrarme" href="/index.php">Ir &raquo;</a></p>
+      </div>
+
+    
+  </div>
+
+
+  </div>
    
-        <th>Nombre</th>
-        <th>Imagen</th>
-        <th>Precio</th>
-        <th>Vencimiento</th>
-        <th>Formula</th>
-        <th>Categoria</th>
-        <th>Descripcion</th>
-        <th>Acciones</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach($productos as $producto) { ?>
-      <tr class="text-center"> 
-        <td> <?php echo $producto->nombre;?> </td>
-
-        <td> 
-        <img src="imagenes/<?php echo $producto->imagen;?>" width="50%">
-        </td>
-
-
-        <td> <?php echo "$".number_format($producto->precio,2, '.' ,'.');?></td>
-        <td> <?php echo $producto->fecha;?></td>
-        <td> <?php echo $producto->formula;?> </td>
-        <td> <?php echo $producto->categoria;?></td>
-        <td> <?php echo $producto->descripcion;?></td>
-        <td> <a class="btn btn-danger" href="?borrar=<?php echo $producto->id; ?>" >Eliminar</a> </td>
-      <tr>
-      <?php  } ?>
-    </tbody>
-  </table>
-  
-
-
-
 </div>
-  
+</div>
+<!--Boton flotante -->
+<div class="conta">
+    <div class="boton">
+      <input type="checkbox" id="btn-mas">
+      <div class="redes">
+          <a href="#"><img src="https://img.icons8.com/material-outlined/25/FFFFFF/facebook-new.png"/></a>
+          <a href="#"><img src="https://img.icons8.com/material-outlined/25/FFFFFF/whatsapp--v1.png"/></a>
+          <a href="../cliente/buscar/index_buscar.php" ><img src="https://img.icons8.com/ios-glyphs/25/FFFFFF/search--v1.png"/></a>
+        </div>
+
+        <div class=" btn-mas">
+          <label for="btn-mas" class="fa fa-plus"></label>
+        </div>
+
+
+
+    </div>
+ 
+  </div>    
+<!-- Fin del boton flotante -->
+
+<script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
     <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>    
     <script src="../bootstrap/js/bootstrap.min.js"></script>
     <script type="module" src="../bootstrap/js/background.js"></script>
 
 </body>
 </html>
-
-
-<?php 
-}
-else{
-  header("location: ../error.php");
-}
-?>
-
